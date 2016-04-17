db3bd98d2bceca1073c4770d36df6be81e333e96
mmloveaa <mandychan1128@gmail.com>
2016-04-17 12:29:37 -0700

    removed the home page image

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $("#mypic").mouseenter(function() {
-        $("#mypic").attr("src","img/profile.png");   
-    });
+    // $("#mypic").mouseenter(function() {
+    //     $("#mypic").attr("src","img/profile.png");
+    // });
     $("#mypic").mouseleave(function() {
-        $("#mypic").attr("src","img/Superpic.jpg");  
+        $("#mypic").attr("src","img/Superpic.jpg");
     });
 });
