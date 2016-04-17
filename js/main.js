f4b75a35a315d3a9d2c7fa2fa905a570948b3b9f
mmloveaa <mandychan1128@gmail.com>
2016-04-16 20:58:12 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#mypic").mouseenter(function() {
+        $("#mypic").attr("src","img/profile.png");   
+    });
+    $("#mypic").mouseleave(function() {
+        $("#mypic").attr("src","img/Superpic.jpg");  
+    });
+});
