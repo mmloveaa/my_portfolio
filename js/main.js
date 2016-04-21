4c9328509dbd62f7270ed0c72871d651dd285f95
mmloveaa <mandychan1128@gmail.com>
2016-04-20 18:38:58 -0700

    Changed to NY Resume

--- js/main.js
+++ js/main.js
@@ -5,32 +5,34 @@ $(document).ready(function() {
     $("#mypic").mouseleave(function() {
         $("#mypic").attr("src","img/Superpic.jpg");
     });
+
+
+    var hideModal = function() {
+        modalContainer.removeClass('show')
+        // modalContainer.hide();
+      };
+
+      var showModal = function() {
+        modalContainer.addClass('show')
+        // modalContainer.show();
+      };
+
+      var showModalButton = $('#modal-show');
+      (showModalButton).on('click', showModal)
+
+      var closeModalButton = $('#modal-hide');
+      closeModalButton.on('click', hideModal);
+
+
+      var submitEmail = function(evt){
+       var thankYouNote = $('#email-header');
+       var thankYouNoteForm = $('#email-signup');
+       thankYouNoteForm.hide();
+       thankYouNote.text("Thanks!")
+       setTimeout(hideModal, 2000);
+     };
+
+     var emailForm = $('#email-signup');
+     emailForm.on('submit', submitEmail)
+
 });
-
-var hideModal = function() {
-    modalContainer.removeClass('show')
-    // modalContainer.hide();
-  };
-
-  var showModal = function() {
-    modalContainer.addClass('show')
-    // modalContainer.show();
-  };
-
-  var showModalButton = $('#modal-show');
-  (showModalButton).on('click', showModal)
-
-  var closeModalButton = $('#modal-hide');
-  closeModalButton.on('click', hideModal);
-
-
-  var submitEmail = function(evt){
-   var thankYouNote = $('#email-header');
-   var thankYouNoteForm = $('#email-signup');
-   thankYouNoteForm.hide();
-   thankYouNote.text("Thanks!")
-   setTimeout(hideModal, 2000);
- };
-
- var emailForm = $('#email-signup');
- emailForm.on('submit', submitEmail)
