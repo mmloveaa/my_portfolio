aa8e911ac291be84188daa83d042cd3050b5ea85
mmloveaa <mandychan1128@gmail.com>
2016-04-20 17:50:34 -0700

    Fixed Resume Modal

--- js/main.js
+++ js/main.js
@@ -6,3 +6,31 @@ $(document).ready(function() {
         $("#mypic").attr("src","img/Superpic.jpg");
     });
 });
+
+var hideModal = function() {
+    modalContainer.removeClass('show')
+    // modalContainer.hide();
+  };
+
+  var showModal = function() {
+    modalContainer.addClass('show')
+    // modalContainer.show();
+  };
+
+  var showModalButton = $('#modal-show');
+  (showModalButton).on('click', showModal)
+
+  var closeModalButton = $('#modal-hide');
+  closeModalButton.on('click', hideModal);
+
+
+  var submitEmail = function(evt){
+   var thankYouNote = $('#email-header');
+   var thankYouNoteForm = $('#email-signup');
+   thankYouNoteForm.hide();
+   thankYouNote.text("Thanks!")
+   setTimeout(hideModal, 2000);
+ };
+
+ var emailForm = $('#email-signup');
+ emailForm.on('submit', submitEmail)
